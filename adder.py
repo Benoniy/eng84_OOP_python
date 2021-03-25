a486b140a6423de4449b268b8cd3d3ab76f64327
Ben <benranson5@gmail.com>
2021-03-25 12:15:45 +0000

    added another example of polymorphism

--- adder.py
+++ adder.py
@@ -7,7 +7,11 @@ class Adder(Snake):
         super().__init__()
         self.venom = False  # You can override values of the parent class, this is polymorphism
 
+    def smell(self):  # You can also override the method of the parent class, this is polymorphism
+        return "I smell but like an adder"
+
 
 if __name__ == "__main__":
     bob = Adder()
     print(bob.venom)
+    print(bob.smell())
